ff9e26da236fe56bb72f3ea8f694eca78ab40862
Felix Ingram <felix.ingram@nccgroup.com>
2020-12-07 09:23:11 +0000

    Update setup.py formatting. Twas a mess

--- setup.py
+++ setup.py
@@ -12,28 +12,23 @@ import pathlib
 here = pathlib.Path(__file__).parent.resolve()
 
 # Get the long description from the README file
-long_description = (here / 'README.md').read_text(encoding='utf-8')
+long_description = (here / "README.md").read_text(encoding="utf-8")
 
 # Arguments marked as "Required" below must be included for upload to PyPI.
 # Fields marked as "Optional" may be commented out.
 
 setup(
-    name='cloud_ip_ranges',  # Required
-
-    version='1.0.0', 
-
+    name="cloud_ip_ranges",  # Required
+    version="1.0.0",
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#summary
-    description="This tools identifies if an IP belongs to a provider's ranges by fetching and parsing the latest lists.",  
-
+    description="This tools identifies if an IP belongs to a provider's ranges by fetching and parsing the latest lists.",
     long_description=long_description,
-    long_description_content_type='text/markdown',  
-
-    url='https://github.com/nccgroup/cloud_ip_ranges', 
-    author='Xavier Garceau-Aranda',
-    author_email='xavier.garceau-aranda@owasp.org',  
-
+    long_description_content_type="text/markdown",
+    url="https://github.com/nccgroup/cloud_ip_ranges",
+    author="Xavier Garceau-Aranda",
+    author_email="xavier.garceau-aranda@owasp.org",
     # Classifiers help users find your project by categorizing it.
     #
     # For a list of valid classifiers, see https://pypi.org/classifiers/
@@ -42,21 +37,16 @@ setup(
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 3 - Beta',
-
-        'Intended Audience :: Developers',
-
+        "Development Status :: 3 - Beta",
+        "Intended Audience :: Developers",
         # Pick your license as you wish
-        'License :: OSI Approved :: MIT License',
-
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3 :: Only',
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3 :: Only",
     ],
-
-
     # You can just specify package directories manually here if your project is
     # simple. Or you can use find_packages().
     #
@@ -68,26 +58,18 @@ setup(
     #
     py_modules=["cloud_ip_ranges"],
     # packages=find_packages(where='src'),  # Required
-
     # Specify which Python versions you support. In contrast to the
     # 'Programming Language' classifiers above, 'pip install' will check this
     # and refuse to install the project if the version does not match. See
     # https://packaging.python.org/guides/distributing-packages-using-setuptools/#python-requires
-    python_requires='>=3.5, <4',
-
+    python_requires=">=3.5, <4",
     # This field lists other packages that your project depends on to run.
     # Any package you put here will be installed by pip when your project is
     # installed, so they must be valid existing projects.
     #
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=
-    ["requests",
-"netaddr",
-"lxml",
-"coloredlogs"],
-
-
+    install_requires=["requests", "netaddr", "lxml", "coloredlogs"],
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
     # `pip` to create the appropriate form of executable for the target
@@ -96,9 +78,6 @@ setup(
     # For example, the following would provide a command called `sample` which
     # executes the function `main` from this package when invoked:
     entry_points={  # Optional
-        'console_scripts': [
-            'cloud_ip_ranges=cloud_ip_ranges:main',
-        ],
+        "console_scripts": ["cloud_ip_ranges=cloud_ip_ranges:main",],
     },
-
 )
